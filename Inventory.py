77fc04be3a2801946c4baf631f789b2c04bbfc1f
AidenB22 <102330169+AidenB22@users.noreply.github.com>
2023-11-08 14:53:13 -0600

    Add files via upload

--- Inventory.py
+++ Inventory.py
@@ -1,8 +1,14 @@
-class Inventory:
+import sqlite3
+class Inventory:  
+    dbName = ""
+    tableName = ""
     def Inventory():
 
-    def Inventory(string ):
 
+    def Inventory(string indbname, string intableName):
+        dbName = indbName
+        tableName = intableName
+    
     def viewInventory():
 
     def searchInventory():
