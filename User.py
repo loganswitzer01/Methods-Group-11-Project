c58afb306f3b255acf11624633935d33a634c265
Logan Switzer <30557653+loganswitzer01@users.noreply.github.com>
2023-11-10 10:30:11 -0600

    Update User.py

--- User.py
+++ User.py
@@ -1,1 +1,39 @@
+import sqlite3
+class User:  
+    dbName = ""
+    tableName = ""
+    loggedIn = false
+    userID = ""
+  
+    def User():
+        loggedIn = false
+        userID = ""
+        
+    def User(string indbname, string intableName):
+        dbName = indbName
+        tableName = intableName
+        loggedIn = false
+        userID = ""
+    
+    def login(string inuserID):
+        userID = inuserID
+        loggedIn = true
+        return true
 
+    def logout():
+        userID = ""
+        loggedIn = false
+        return false
+
+    def viewAccountInformation():
+        print("userID: ", userID)
+
+    def createAccount():
+
+    def getLoggedIn(string inloggedIn):
+        loggedIn = inloggedIn
+
+    def getUserID(string inuserID):
+        userID = inuserID
+
+    
