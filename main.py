2206ed374f5ffdb235575aa14be46f95a46e8863
AidenB22 <102330169+AidenB22@users.noreply.github.com>
2023-11-16 17:34:51 -0600

    Update main.py
    Just updating the initial formatting.  Will add functions later

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sqlite3
 
 class main:
-print("Login Page")
+  print("Welcome to the Book Nook! \n (1) Login \n (2) Create Account \n (3) Logout")
+
+  preLogin = input(" Please choose an option here: ")
+  inPreLogin = int(preLogin)
+
+  if(inPreLogin == 1):
+      print("\nWelcome to the Book Nook!")
+  elif(inPreLogin == 2):
+      print("\nCreating Account")
+  elif(inPreLogin == 3):
+      print("\nYou have successfully Logged Out")
+  else:
+      print("Invalid Response...")
 
