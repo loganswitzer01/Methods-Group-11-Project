2aeb11f999fc32408307f090d6a7d995938c013d
rushingbrody <149844219+rushingbrody@users.noreply.github.com>
2023-11-29 16:05:33 -0600

    Add files via upload

---
+++ cart.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION:
+
+CREATE TABLE IF NOT EXISTS "Cart" (
+       "cart_id" INTEGER NOT NULL UNIQUE,
+       "user_id" INTEGER NOT NULL UNIQUE,
+       "quanity" INTEGER NOT NULL,
+       "book_id" INTEGER NOT NULL,
+       PRIMARY KEY("cart_id")
+
+);
+INSERT INTO "Cart" VALUES (0001, 'agb44@msstate.edu','', Aiden', 
