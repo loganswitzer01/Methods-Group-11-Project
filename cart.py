f4c65bea00c2e69b0f0103eeff956ebf9e26036a
rushingbrody <149844219+rushingbrody@users.noreply.github.com>
2023-11-29 19:54:28 -0600

    Update cart.py
    most up to date code

--- cart.py
+++ cart.py
@@ -4,12 +4,29 @@ from tokenize import String
 
 
 class cart:
-    def _init_(self, user_id, cart_id, quantiy, book_name, book_id):
-        self.user_id = user_id
-        self.cart_id = user_id + 69
-        self.quanity = 0
-        self.book_name = []
-        self.book_id = []
+    #test
+    dbName = ""
+    tableName = ""
+    #end test 
+    
+    book_name = []
+    book_id = []
+    quanity = 0
+    
+    #test
+    def cart(self):
+    	dbName = ""
+    	tableName = ""
+    def cart(self, indbName, intableName):
+    	dbName = indbName
+    	tableName = intableName
+ 
+ #   def _init_(self, user_id, cart_id, quantiy, book_name, book_id):
+ #       self.user_id = user_id
+ #       self.cart_id = user_id + 69
+ #       self.quanity = 0
+ #       self.book_name = []
+ #       self.book_id = []
 
     #view the items in the cart
     def view(self):
@@ -27,26 +44,28 @@ class cart:
         else:
             
             #connects the cursor to write to the table
-            cursor = connection.currsor()
-            cursor.execute("SELECT * FROM Cart")
-            result = curcor.fetchall()
+           #/ cursor = connection.cursor()
+           
+           #/ cursor.execute("SELECT * FROM cart")
+           #/ result = cursor.fetchall()
 
             #inputs in all the items into the db
+            print("\n")
             for x in self.book_name:
-                print("Title:", x[1])
+                print("Title:", x)
 
             #closes the cursor
-            cursor.close()
-            connection.close()
+           #/ cursor.close()
+           #/ connection.close()
 
     #add item to the cart
-    def add(self, bname, bid, number):
-        bname = input("\n What book would you like to add: \n")
+    def add(self):
+        bname = str(input("\n What book would you like to add: \n"))
         number = 0
 
         #makes sure the user can not add a negative number
         while number == 0:
-             number = input("How many would you like to add: \n")
+             number = int(input("How many would you like to add: \n"))
              if number < 0:
                 number = 0
             
@@ -62,18 +81,18 @@ class cart:
         #adds items to the db
         while number != 0:
             self.book_name.append(bname)
-            self.book_id.append(bid)
+            #self.book_id.append(bid)
             self.quanity +=1
             number -=1
     	
 
     # need to look to see if it only remove one if there are duplicites
-    def remove(self, bname, bid):
+    def remove(self):
         bname = input("\n What book would you like to remove: \n")
         
         if bname in self.book_name:
             self.book_name.remove(bname)
-            self.book_id.remove(bid)
+            #self.book_id.remove(bid)
             self.quanity -=1
         else:
             print("\nBook is not in cart\n")
@@ -103,12 +122,13 @@ class cart:
                     number = 0
                 
 
-    def checkOut(cart):
+  
 
-    def decreaseStock(cart):
-
+   # def decreaseStock(self):
+    
     def clear(self):
         self.book_name.clear()
         self.book_id.clear()
-
+        
+        self.quanity = 0
         
